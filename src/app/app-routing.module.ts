3933f9989b26ac83b568d696931ea6f3ec8a10cd
EWimmer <elisabeth.wimmer@uni-ulm.de>
2024-08-13 23:11:30 +0200

    Created a Login Screen and a Registration Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,9 +12,27 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+
+
 ];
 
 @NgModule({
