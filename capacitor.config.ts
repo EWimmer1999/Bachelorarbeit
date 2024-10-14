100ff470fc4dd3754fde9142df740a1b5af12b8a
EWimmer <elisabeth.wimmer@uni-ulm.de>
2024-10-14 21:27:10 +0200

    tester

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  server: {
-    androidScheme: 'http',
-    cleartext: true
-  },
-  android: {
-    allowMixedContent: true
-  },
+  appId: 'io.ionic.starter',
+  appName: 'Bachelorarbeit',
+  webDir: 'www'
 };
 
 export default config;
-
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {StepCounter} from 'capacitor-stepcounter';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
+
 export class HomePage implements OnInit {
+  steps: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.startStepCounter();
   }
 
+  async startStepCounter() {
+    await StepCounter.start();
+    this.updateStepCount();
+  }
+
+  async updateStepCount() {
+    setInterval(async () => {
+      const result = await StepCounter.getStepCount();
+      this.steps = result.steps;
+    }, 1000); // Aktualisiert den Schrittzähler alle 1 Sekunde
+  }
+
+  async stopStepCounter() {
+    await StepCounter.stop();
+  }
 }
