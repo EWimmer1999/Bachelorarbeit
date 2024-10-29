e45396992f539c6286c91839109698f3a1ddedb6
EWimmer <elisabeth.wimmer@uni-ulm.de>
2024-10-29 09:36:13 +0100

    feat: App now supports Android Foreground Service

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,25 +1,5 @@
 package io.ionic.starter;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin; // Importiere Plugin, um die Annotation zu verwenden
-import io.capacitor.plugin.stepcounter.StepCounterPlugin; // Importiere dein Plugin
-import io.capacitor.plugin.noisemeter.NoiseMeterPlugin;
 
-import java.util.ArrayList;
-
-
-public class MainActivity extends BridgeActivity {
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-
-    registerPlugin(StepCounterPlugin.class);
-
-    registerPlugin(NoiseMeterPlugin.class);
-
-    super.onCreate(savedInstanceState);
-
-  }
-}
-
+public class MainActivity extends BridgeActivity {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,14 +4,13 @@ import { NoiseMeter } from 'capacitor-noisemeter';
 import { StorageService } from 'src/app/services/storage.service';
 import { UpdateService } from 'src/app/services/update.service';
 import { ThemeService } from 'src/app/services/theme.service';
+import { Foregroundservice } from 'capacitor-foregroundservice';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-
-
 export class HomePage implements OnInit, OnDestroy {
   steps: number = 0;
   interval: any;
@@ -19,16 +18,21 @@ export class HomePage implements OnInit, OnDestroy {
   averageNoise: number = 0;
   currentDecibels: number = 0;
   noised: boolean = false;
-
   isMeasuring: boolean = false;
 
   constructor(
-    private storage: StorageService, 
+    private storage: StorageService,
     private updateService: UpdateService,
     private themeService: ThemeService
-  ) {} 
+  ) {}
 
   async ngOnInit() {
+    try {
+      await Foregroundservice.startService();
+    } catch (error) {
+      console.log('Fehler beim Starten des Foreground Services:', error);
+    }
+
     await this.storage.initStorage();
     await this.startStepCounter();
     this.updateStepCount();
@@ -36,22 +40,32 @@ export class HomePage implements OnInit, OnDestroy {
     this.interval = setInterval(() => {
       this.updateStepCount();
     }, 5000);
+    
     await this.updateService.getSettings();
     this.themeService.applyTheme();
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.themeService.applyTheme();
-    this.updateService.updateApp()
+    this.updateService.updateApp();
   }
 
   async ionViewDidLeave() {
     if (this.noised) {
-      this.stopNoiseMeter()
+      await this.stopNoiseMeter();
       this.noised = false;
     }
-
   }
+
+  async testForeground() {
+    try {
+      await Foregroundservice.startService();
+    } catch (error) {
+      console.log('Fehler beim Testen des Foreground Services:', error);
+    }
+  }
+
+
 
   async toggleNoiseMeter() {
     if (this.isMeasuring) {
@@ -75,7 +89,7 @@ export class HomePage implements OnInit, OnDestroy {
       const result = await StepCounter.getStepCount();
       this.steps = result.steps;
     } catch (error) {
-      alert('Fehler beim Abrufen der Schrittanzahl: ' + JSON.stringify(error));
+      console.log('Fehler beim Abrufen der Schrittanzahl:', error);
     }
   }
 
@@ -87,7 +101,7 @@ export class HomePage implements OnInit, OnDestroy {
       }, 1000);
       this.noised = true;
     } catch (error) {
-      alert('Fehler beim Starten des NoiseMeters: ' + JSON.stringify(error));
+      console.log('Fehler beim Starten des NoiseMeters:', error);
     }
   }
 
@@ -95,7 +109,7 @@ export class HomePage implements OnInit, OnDestroy {
     try {
       const result = await NoiseMeter.getNoiseLevel();
       this.currentDecibels = result.decibels;
-      
+
       if (!isNaN(this.currentDecibels)) {
         this.noiseData.push(this.currentDecibels);
       } else {
@@ -105,7 +119,7 @@ export class HomePage implements OnInit, OnDestroy {
       console.log('Fehler beim Abrufen des Geräuschpegels:', error);
     }
   }
-  
+
   async stopNoiseMeter() {
     try {
       clearInterval(this.interval);
@@ -114,39 +128,41 @@ export class HomePage implements OnInit, OnDestroy {
       this.saveAverageNoiseWithDate();
       this.noised = false;
     } catch (error) {
-      alert('Fehler beim Stoppen des NoiseMeters: ' + JSON.stringify(error));
+      console.log('Fehler beim Stoppen des NoiseMeters:', error);
     }
   }
 
   private calculateAverageNoise() {
     if (this.noiseData.length === 0) {
-      this.averageNoise = 0; // Falls keine Daten vorhanden sind
+      this.averageNoise = 0;
       return;
     }
-  
+
     const sum = this.noiseData.reduce((acc, val) => acc + val, 0);
     this.averageNoise = sum / this.noiseData.length;
   }
-  
 
   private async saveAverageNoiseWithDate() {
     const date = new Date().toISOString();
     const savedData = {
       date: date,
-      averageNoise: this.averageNoise
+      averageNoise: this.averageNoise,
     };
-  
+
     console.log('Zu speichernde Daten:', savedData);
     await this.storage.set(date, savedData);
     console.log('Gespeicherte Daten:', savedData);
   }
-  
 
   async ngOnDestroy() {
     if (this.interval) {
       clearInterval(this.interval);
     }
-    await StepCounter.stop();
-    await NoiseMeter.stop();
+    try {
+      await StepCounter.stop();
+      await NoiseMeter.stop();
+    } catch (error) {
+      console.log('Fehler beim Zerstören:', error);
+    }
   }
 }
